5c4d7892c1baeff971e9f7fd6d7dc781f25b5c3c
a24yaniravc <a24yaniravc@iessanclemente.net>
2025-01-15 14:34:26 +0100

    New ex+

---
+++ ex2-colorpicker/javascript.js
@@ -0,0 +1,6 @@
+function color_picker(){
+    let picker = document.getElementById("picker");
+    let square = document.getElementById("square");
+    
+    square.style.backgroundColor=picker.value;
+}
--- test1/javascript.js
+++ test1/javascript.js
@@ -14,3 +14,21 @@ function changeText(){
     let paragraph = document.getElementById("para");
     paragraph.textContent = "no";
 }
+
+
+// Se puede establecer como "boton" cualquier cosa
+function seeInfo(){
+    let paragraph2=document.getElementById("para2");
+    document.write(paragraph2.innerHTML);
+}
+
+/*
+---------------------------------------------
+Llevando solo texto (sin el html como tal):
+---------------------------------------------
+function seeInfo(){
+    let paragraph2=document.getElementById("para2");
+    document.write(paragraph2.textContent);
+}
+---------------------------------------------
+*/
