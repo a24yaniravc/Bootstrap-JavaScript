4e5861dbbbf1fa8107c3452add7352d009cc4de7
a24yaniravc <a24yaniravc@iessanclemente.net>
2025-01-20 17:37:42 +0100

    Intento2

--- ex3-darkmode/javascript.js
+++ ex3-darkmode/javascript.js
@@ -1,1 +1,14 @@
-MOUSEOVER!!!
+
+let image = document.getElementById("imagen");
+
+image.addEventListener("mouseover", function() {
+    image.style.opacity= "100";
+})
+
+image.addEventListener("mouseout", function() {
+    image.style.opacity= "0";
+})
+
+
+
+MOUSEOVER
