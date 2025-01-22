07565302d64acf56cf4be162ee4bab2f0e1df4cd
a24yaniravc <a24yaniravc@iessanclemente.net>
2025-01-22 19:44:18 +0100

    Arregaldo ej 3

--- ex3-darkmode/javascript.js
+++ ex3-darkmode/javascript.js
@@ -8,12 +8,12 @@ image.addEventListener("mouseout", function() {
     image.style.opacity= 0;
 });*/
 
-function incrementOpacity(){
-    let image = document.getElementById("imagen");
-    image.style.opacity=1;
+function incrementOpacity(id) {
+    let image = document.getElementById(id);
+    image.style.opacity = 1;
 }
 
-function decreaseOpacity(){
-    let image = document.getElementById("imagen");
-    image.style.opacity=0;
+function decreaseOpacity(id) {
+    let image = document.getElementById(id);
+    image.style.opacity = 0;
 }
