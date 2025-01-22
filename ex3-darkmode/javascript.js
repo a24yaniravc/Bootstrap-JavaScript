d7b7eb1d203c29947c9737d0ecad2f7037e55f18
a24yaniravc <a24yaniravc@iessanclemente.net>
2025-01-22 14:30:56 +0100

    Cositas

--- ex3-darkmode/javascript.js
+++ ex3-darkmode/javascript.js
@@ -1,14 +1,19 @@
 
-let image = document.getElementById("imagen");
 
-image.addEventListener("mouseover", function() {
-    image.style.opacity= "100";
-})
+/*image.addEventListener("mouseover", function() {
+    image.style.opacity= 1;
+});
 
 image.addEventListener("mouseout", function() {
-    image.style.opacity= "0";
-})
+    image.style.opacity= 0;
+});*/
 
+function incrementOpacity(){
+    let image = document.getElementById("imagen");
+    image.style.opacity=1;
+}
 
-
-MOUSEOVER
+function decreaseOpacity(){
+    let image = document.getElementById("imagen");
+    image.style.opacity=0;
+}
